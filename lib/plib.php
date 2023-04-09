47ebfc649640e6fee91c78d0b5376afa2aeb510b
denirazedeni <denirazedeni@gmail.com>
2023-04-09 16:42:19 +0200

    Created Class Include File

--- lib/plib.php
+++ lib/plib.php
@@ -3,6 +3,7 @@
 // Includes Area
 include_once $_SERVER['DOCUMENT_ROOT'].'/lib/urls.php';
 include_once $_SERVER['DOCUMENT_ROOT'].'/lib/db.php';
+include_once $_SERVER['DOCUMENT_ROOT'].'/lib/classes.php';
 
 // Vars Area
 $REQUESTED_PATH = null;
