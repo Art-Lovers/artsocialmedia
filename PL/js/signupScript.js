e8eb472d695ff9c8c0532215d1208bdf5dba22ee
andriaaaa <andriaqurku@yahoo.com>
2023-04-11 09:25:06 +0200

    input validation

--- PL/js/signupScript.js
+++ PL/js/signupScript.js
@@ -1,14 +1,33 @@
 $(document).ready(function(){
     $("#signupForm").on("submit", function(){
-    
+    userError=false;
     if(!($('.input[name="username"]').val().match(/^[A-Za-z0-9]+$/)) ){
         $('#userErr').text("Username must be alphanumeric. No special characters allowed.");
+        userError=true;
     }
-    else if(!($('.input[name="firstname"]').val().match(/^[A-Za-z0-9]+$/))){
-        $('#nameErr').text("Name must me alphanumeric. No special characters allowed.")
+    else { $('#userErr').text("");}
+
+    nameError=false;
+    if(!($('.input[name="firstname"]').val().match(/^[A-Za-z0-9]+$/))){
+        $('#nameErr').text("Name must me alphanumeric. No special characters allowed.");
+        nameError=true;
     }
-    else if(!($('.input[name="lastname"]').val().match(/^[A-Za-z0-9]+$/))){
-        $('#nameErr').text("Name must me alphanumeric. No special characters allowed.")
+    else {$('#nameErr').text("");}
+    
+    lastNameError=false;
+    if(!($('.input[name="lastname"]').val().match(/^[A-Za-z0-9]+$/))){
+        $('#nameErr').text("Name must me alphanumeric. No special characters allowed.");
+        lastNameError=true;
+    }
+    else {$('#nameErr').text("");}
+    
+    passError=false;
+    if(!($('.input[name="password"]').val().match(/^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/)) ){
+        $('#passError').text("Password must have 1 lowercase and 1 uppwecase letter, 1 number and 1 special character (!@#$%^&*)");
+        passError=true;
+    }
+    else{
+        $('#passError').text("");
     }
 
     $.post("ajax/ajaxAuthenticate.php",{ajaxCall: 'signup',data:JSON.stringify( $("#signupForm").serializeArray() )}, function(data, status){
