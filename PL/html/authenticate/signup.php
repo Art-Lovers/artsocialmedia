4a64538bc48a058ddbe547ac1d947232e4e67c88
Ana Dhimitri <anadhimitri08@gmail.com>
2023-05-01 16:53:12 +0200

    few profile changes

--- PL/html/authenticate/signup.php
+++ PL/html/authenticate/signup.php
@@ -12,38 +12,38 @@
         <header>Registration Form</header>
         <form action="" method="post" class="form" id="signupForm">
             <div class="input_box">
-                <label for="">First Name</label>
+                <label for="">First Name *</label>
                 <input type="text" class="input name" name="firstname" placeholder="Enter your name" required>
                 <label class="error" id='nameErr'></label>
             </div>
             <div class="input_box">
-                <label for="">Last Name</label>
+                <label for="">Last Name *</label>
                 <input type="text" class="input" name="lastname" placeholder="Enter your last name" required>
                 <label class="error" id='lastNameErr'></label>
             </div>
             <div class="input_box">
-                <label for="">Username</label>
+                <label for="">Username *</label>
                 <input type="text" name="username" class="input user1" placeholder="Enter your username" required>
                 <label class="error" id='userErr'></label>
             </div>
             <div class="column">
                 <div class="input_box">
-                    <label for="">Email Address</label>
+                    <label for="">Email Address *</label>
                     <input type="email" name="email" placeholder="Enter your email address" required>
                     <label class="error" id='emailErr'></label>
                 </div>
                 <div class="input_box">
-                    <label for="">Phone number</label>
+                    <label for="">Phone number *</label>
                     <input type="text" name="phoneNumber" placeholder="Enter your phone number" min="0">
                     <label class="error" id='phoneNumErr'></label>
                 </div>
             </div>
             <div class="input_box">
-                <label for="">Birth Date</label>
+                <label for="">Birth Date *</label>
                 <input type="date" name="birthDate" placeholder="Enter your birth date " required>
             </div>
             <div class="gender-box">
-                <h3>Gender</h3>
+                <h3>Gender *</h3>
                 <div class='gender-option'>
                     <div class='gender'>
                         <input type="radio" id="check-male" name=gender value="m" checked>
@@ -62,13 +62,13 @@
                 </div>
             </div>
             <div class="input_box">
-                <label for="">Create Password</label>
+                <label for="">Create Password *</label>
                 <input type="password" name="password" class="input" placeholder="minimum length is 8 characters" minlength="8"
                     maxlength="30" required>
                 <label class="error" id='passwordErr'></label>
             </div>
             <div class="input_box">
-                <label for="">Confirm Password</label>
+                <label for="">Confirm Password *</label>
                 <input type="password" name="confirmpassword" class="input" placeholder="Confirm Password" minlength="8"
                     maxlength="30" required>
                     <label class="error" id='passconfirmfErr'></label>
--- PL/html/profile/view.php
+++ PL/html/profile/view.php
@@ -3,14 +3,30 @@
 
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title> Login Page </title>
+    <title> Profile Page <?php echo $userProfile; ?></title>
+    <link rel="stylesheet" type="text/css" href="PL/css/homepage/homepageStyle.css?v=<?php echo $GLOBAL_SCRIPTS_VER; ?>">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+
+
 </head>
 
 <body>
     <center>
         <h1> Post Form </h1>
-        <h3><?php echo $userProfile; ?></h3>
+        
     </center>
+    <div class="col-2 ml-3">
+                <form id="postForm">
+                    <br>
+
+                    <textarea id="postContent"></textarea><br>
+                    <input type="file" name="fileToUpload" id="attachMediaPost" >
+                    <!-- <button id="">Add photo/video</button><br> -->
+                    <button id="createPostButton">Create post</button>
+                </form>
+                </div>
+
 </body>
 
 </html>
