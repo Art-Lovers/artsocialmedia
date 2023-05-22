9d10625b2f1a87abb9be144ca9a50001cdba6c67
denirazedeni <denirazedeni@gmail.com>
2023-05-22 22:48:58 +0200

    small change

--- PL/html/common/header.php
+++ PL/html/common/header.php
@@ -17,7 +17,7 @@
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
         <div class="col-2">
             <ul class="navbar-nav">
-                <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Home</a></li>
+                <li class="nav-item"><a href="/homepage" class="nav-link active" aria-current="page">Home</a></li>
                 <li class="nav-item"><a href="#" class="nav-link">Friends</a></li>
             </ul>
         </div>
@@ -29,7 +29,7 @@
         <div class="col-2">
             <ul class="navbar-nav">
                 <li class="nav-item"><a href="#" class="nav-link">Notifications</a></li>
-                <li class="nav-item"><a href="#" class="nav-link">Profile</a></li>
+                <li class="nav-item"><a href="/profile" class="nav-link">Profile</a></li>
                 <li class="nav-item"><a href="#" id="logout" class="nav-link">Log Out</a></li>
             </ul>
         </div>
